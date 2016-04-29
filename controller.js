b7a40c8838aa530484e4a3b55bf3e439718245e5
Alva Thomson <alva.thomson@yahoo.com>
2016-04-30 00:26:12 +0700

    change json request implementation

--- controller.js
+++ controller.js
@@ -3,45 +3,62 @@ var parittaApp = angular.module('parittaApp' , []);
 
 parittaApp.controller('parittaCtrl' , function($scope,$http){
 
-	$scope.parittaList =[{'number' : 1 , 'nama' : 'Vandana' },
-						{'number' : 2 ,'nama' :'Tisarana'},
-						{'number' : 3 , 'nama' : 'Pancasila' },
-						{'number' : 4 ,'nama' :''}];
-	$scope.parittaNumber = 1;
+	var isFinishedLoad   = false;
+	$scope.paritta;
+	$scope.parittaTitle  = 'Paritta';
+	$scope.parittaPrev	 = '';
+	$scope.parittaNext	 = '';
+	$scope.parittaNumber = -1;
+	$scope.parittaList   = [];
+
+	$scope.init = function() {
+		$http.get("paritta.json").success(function(response) {
+			$scope.paritta 		= response.paritta;
+			var number = 0;
+
+			angular.forEach($scope.paritta, function(paritta) {
+				$scope.parittaList.push({'number' : number++, 'name' : paritta.name});
+			});
+
+			isFinishedLoad = true;
+		});
+	} 
 
 	$scope.update = function ($pNumber){
-		$http.get("paritta.json").success(function(response) {
-			$scope.nama = response.paritta[$pNumber].nama;
-			$scope.pali = response.paritta[$pNumber].pali;
-			$scope.indo = response.paritta[$pNumber].indo;
+		if (isFinishedLoad && $pNumber >= 0 && $pNumber < $scope.paritta.length) {
+			$scope.name 		= $scope.paritta[$pNumber].name;
+			$scope.parittaTitle = $scope.name;
+			$scope.pali 		= $scope.paritta[$pNumber].pali;
+			$scope.indo 		= $scope.paritta[$pNumber].indo;
 
-			$scope.parittaNumber = $pNumber;
-		});
+			$scope.parittaPrev	 = ($pNumber > 0 ? $scope.paritta[$pNumber - 1].name : '');
+			$scope.parittaNext	 = ($pNumber < $scope.paritta.length - 1 ? $scope.paritta[$pNumber + 1].name : '');
+
+			$scope.parittaNumber = $pNumber;	
+		}
 	}
 
 	$scope.updatePrev = function ($pNumber){
-		$pNumber -= 1;
-		$scope.update($pNumber);
+		if (isFinishedLoad && $scope.parittaNumber >= 0) {		// disable navigation button when no paritta has been selected
+			$pNumber -= 1;
+			$scope.update($pNumber);
+		}
 	}
 
 	$scope.updateNext = function ($pNumber){
-		$pNumber += 1;
-		
-		$scope.update($pNumber);
+		if (isFinishedLoad && $scope.parittaNumber >= 0) {		// disable navigation button when no paritta has been selected
+			$pNumber += 1;
+			$scope.update($pNumber);
+		}
 	}
 
 	$scope.goToParitta = function($pNumber) {
-		$('#paritta-list').addClass('hidden');
-		$('#paritta').removeClass('hidden');
-
-		$scope.update($pNumber);
+		if (isFinishedLoad) {
+			$('#paritta-list').addClass('hidden');
+			$('#paritta').removeClass('hidden');
+			$scope.update($pNumber);
+		}
 	}
 
-	$http.get("paritta.json").success(function(response) {
-		$scope.nama = response.paritta[$pNumber].nama;
-		$scope.pali = response.paritta[$pNumber].pali;
-		$scope.indo = response.paritta[$pNumber].indo;
-
-		$scope.parittaNumber = $pNumber;
-	});
+	$scope.init();
 });
